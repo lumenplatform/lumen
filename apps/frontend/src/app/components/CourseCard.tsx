d49282975dd33106c3357e20245c25feaa41fd9a
wathsala495 <sumuduwathsala80@gmail.com>
2022-07-05 09:39:48 -0700

    fearture(cource card)

--- apps/frontend/src/app/components/CourseCard.tsx
+++ apps/frontend/src/app/components/CourseCard.tsx
@@ -1,4 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Margin } from '@mui/icons-material';
 
 export default function CourseCard(props: any) {
-  return <div>Course Card</div>;
+  return <div><Card sx={{ width:350 }}>
+  <CardActionArea>
+    <CardMedia
+      component="img"
+      height="140"
+     image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRLJVGf2FyO7PH2UPREdmXQH7at_MIqy-g9cu9kdphiLYzlRf5BogQtvs8ucVJtKE5GsY&usqp=CAU"
+    
+     alt="green iguana"
+     
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h6" component="div">
+        SCS3208 Software Project Management
+      </Typography>
+     
+    </CardContent>
+  </CardActionArea>
+</Card>
+</div>;
 }
--- apps/frontend/src/app/pages/management/Dashboard.tsx
+++ apps/frontend/src/app/pages/management/Dashboard.tsx
@@ -1,5 +1,14 @@
 import Box from '@mui/material/Box';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Margin } from '@mui/icons-material';
 
 export default function Dashboard() {
-  return <Box sx={{ display: 'flex' }}>Dashboard with stats</Box>;
+  return <Box sx={{ display: 'flex' }}>Dashboard with stats
+
+</Box>;
 }
