7d79bebdba4a10b6284e32bd85085c44a3ed7550
RuwaniWelewatta <77968366+RuwaniWelewatta@users.noreply.github.com>
2022-07-08 22:20:36 +0530

    feat(student):Implemented StudentHomeContainer (#28)

--- apps/frontend/src/app/pages/student/StudentHome.tsx
+++ apps/frontend/src/app/pages/student/StudentHome.tsx
@@ -1,16 +1,76 @@
-import { Link } from 'react-router-dom';
-import CourseCard from '../../components/CourseCard';
-import UpcomingEvents from '../../components/UpcomingEvents';
+import {
+  Box,
+  Breadcrumbs,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+  useTheme,
+  Link,
+  Avatar,
+} from '@mui/material';
+import { NavLink, Outlet } from 'react-router-dom';
+import StudentHeader from '../../components/StudentHeader';
 
-export default function StudentHome(props: any) {
+
+export default function CoursePage(props: any) {
   return (
     <div>
-      <Link to="/">Site Home</Link>
-      <h1>Student Home</h1>
-      <Link to="/student/course-01">Course 01</Link>
+      <StudentHeader />
+      <Box sx={{ maxWidth: '1440px', px: 3 }}>
+        <Box sx={{ py: 1 }}>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/student">
+              Home
+            </Link>
+            <Link underline="hover" color="inherit" href="/student/">
+              Student Home Page
+            </Link>
+          </Breadcrumbs>
+        </Box>
+        <Box sx={{ py: 1, display: 'flex' }}>
+          <Box>
+            <Typography variant="h5" lineHeight={1}>
+              Student Home Page
+            </Typography>
+          </Box>
+        </Box>
+        
+        <Box sx={{ display: 'flex', margin: '', backgroundColor: '#EBE8E1', height: '200px', width: '1150px'}}>
+          <Box sx={{ p: 2 , flex:1,mr:10}}>
+            <Outlet />
+          </Box>
+        </Box>
+        
+        <br />
+        <Box sx={{ display: 'flex', marginLeft: '950px', backgroundColor: '#EBE8E1', height: '50px', width: '200px'}}>
+          <Box sx={{ p: 2 , flex:1,mr:10}}>
+            <Outlet />
+          </Box>
+        </Box>
 
-      <CourseCard></CourseCard>
-      <UpcomingEvents />
+        <br />
+
+        <Box sx={{ display: 'flex', margin: '', backgroundColor: '#EBE8E1', height: '500px', width: '1150px'}}>
+          <Box sx={{ p: 2 , flex:1,mr:10}}>
+            <Outlet />
+          </Box>
+        </Box>
+        
+      </Box>
+
+      <Box sx={{ display: 'flex', marginLeft: '1200px', marginTop: '-800px', backgroundColor: '#EBE8E1', height: '200px', width: '270px', alignContent: 'left'}}>
+        <Box sx={{ p: 2 , flex:1,mr:10}}>
+          <Outlet />
+        </Box>
+      </Box>
+      <br />
+      <Box sx={{ display: 'flex', marginLeft: '1200px', marginTop: '0px', backgroundColor: '#EBE8E1', height: '580px', width: '270px', alignContent: 'left'}}>
+        <Box sx={{ p: 2 , flex:1,mr:10}}>
+          <Outlet />
+        </Box>
+      </Box>
     </div>
   );
 }
