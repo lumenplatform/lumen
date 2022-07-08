a1c545b327f2cfed1b17113d1d46e6f31e659bbf
wathsala495 <sumuduwathsala80@gmail.com>
2022-07-08 02:07:08 -0700

    feat:StatCard

--- apps/frontend/src/app/pages/management/Dashboard.tsx
+++ apps/frontend/src/app/pages/management/Dashboard.tsx
@@ -1,14 +1,10 @@
 import Box from '@mui/material/Box';
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import { Margin } from '@mui/icons-material';
+import StatCard from '../../components/StatCard';
+
 
 export default function Dashboard() {
-  return <Box sx={{ display: 'flex' }}>Dashboard with stats
-
+  return <Box sx={{ display: 'flex' }}>
+  <StatCard></StatCard>
 </Box>;
 }
