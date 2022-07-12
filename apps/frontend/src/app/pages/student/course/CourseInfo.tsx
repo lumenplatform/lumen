6c2f237735b3c9923d81dda446cdccd7728ecc8f
wathsala495 <84239508+wathsala495@users.noreply.github.com>
2022-07-12 03:12:14 -0700

    feat:StatCard (#33)

--- apps/frontend/src/app/pages/student/course/CourseInfo.tsx
+++ apps/frontend/src/app/pages/student/course/CourseInfo.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import BallotIcon from '@mui/icons-material/Ballot';
 
-function CourseInfo() {
-  return <div>CourseInfo</div>;
+function CourseInfo( 
+  
+) {
+  return <div>
+        <TableContainer component={Paper}>
+     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+      <TableHead>
+        <TableRow>
+        
+           <TableCell>Course Id</TableCell>
+           <TableCell>01</TableCell>
+
+        </TableRow>
+     </TableHead>
+     <TableHead>
+        <TableRow>
+           <TableCell>Course Name</TableCell>
+           <TableCell>Software Project mangement</TableCell>
+
+        </TableRow>
+     </TableHead>
+     <TableHead>
+        <TableRow>
+           <TableCell>Language</TableCell>
+           <TableCell>English</TableCell>
+
+        </TableRow>
+     </TableHead>
+     <TableHead>
+        <TableRow>
+           <TableCell>Course description</TableCell>
+           <TableCell></TableCell>
+
+        </TableRow>
+     </TableHead>
+    
+     </Table>
+     </TableContainer>
+  </div>;
 }
 
 export default CourseInfo;
