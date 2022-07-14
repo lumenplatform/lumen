b19b32498a95f523e6da48b52e3f92833bc80b90
Dalana Dharmathilake <43986725+dalanad@users.noreply.github.com>
2022-07-14 13:10:32 +0530

    feat(backend): setup orm (#35)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 import * as express from 'express';
 import * as path from 'path';
+var morgan = require('morgan');
+
+import { InitORM } from './app/config/db';
 
 import { indexRouter } from './app/routes';
+import { logger } from './app/utils/logger';
 import { environment } from './environments/environment';
 
 declare global {
@@ -18,14 +22,25 @@ declare global {
 const app = express();
 
 if (environment.production) {
+  // prod only middleware
   app.use(express.static(path.join(__dirname, '../frontend')));
+} else {
+  // dev only middleware
+  app.use(
+    morgan(
+      '[REQUEST] :method :url :status :response-time ms - :res[content-length]',
+      {}
+    )
+  );
 }
 
 app.use('/api', indexRouter);
 
 const port = process.env.PORT;
 
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+InitORM().then((e) => {
+  const server = app.listen(port, () => {
+    logger.info(`Listening at http://localhost:${port}/api`);
+  });
+  server.on('error', logger.error);
 });
-server.on('error', console.error);
