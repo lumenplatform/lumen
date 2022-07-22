5985c1b8651f5c75685b32752d18ee2ce88014ee
dalanad <dalana199981@gmail.com>
2022-07-22 23:22:49 +0530

    feat(backend): course controller

--- apps/backend/src/app/routes/courses.router.ts
+++ apps/backend/src/app/routes/courses.router.ts
@@ -1,6 +1,11 @@
+import { RequestContext } from '@mikro-orm/core';
 import * as express from 'express';
-
+import { CourseController } from '../controllers/course.controller';
+import { CourseMaterial } from '../models/course-material.model';
+import { createResponse } from './../utils/response-mapper';
 export const coursesRouter = express.Router();
+
+const courseController = new CourseController();
 
 // search courses
 coursesRouter.get('/');
