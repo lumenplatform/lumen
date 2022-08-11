501447c3b655d7cb70ed57175c6e42e9946f6852
predator1999 <73787312+predator1999@users.noreply.github.com>
2022-08-11 22:09:17 +0530

    Feat/home page (#65)
    * feat(enhancement) : Homepage UI
    
    * feat(bug):exampage fix
    
    * feat(bug): fix
    
    * feat(course): remove homepage icons
    
    Co-authored-by: ashanhr <ashanhr@gmail.com>

--- apps/frontend/src/app/components/StatCard.tsx
+++ apps/frontend/src/app/components/StatCard.tsx
@@ -1,28 +1,34 @@
-import { CardActionArea } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import { Grid, Paper, Typography, useTheme } from '@mui/material';
 
 export default function StatCard(props: any) {
+  const { image, heading, subheading, stat, caption,imageSize } = props;
+  const theme = useTheme();
   return (
-    <div>
-      <Card
-        sx={{
-          height: '150px',
-          borderRadius: 4,
-        }}
-        elevation={2}
+    <Paper
+      elevation={2}
+      sx={{
+        height: '150px',
+        background: image,
+        backgroundRepeat: 'no-repeat',
+        backgroundPositionX: 'right',
+        backgroundPositionY: 'bottom',
+        backgroundSize: imageSize,
+        p: 3,
+        borderRadius: theme.shape.borderRadius,
+      }}
+    >
+      <Typography variant="h6"> {heading}</Typography>
+      <Typography variant="h4"> {subheading}</Typography>
+      <Typography
+        fontWeight="700"
+        fontSize={'2rem'}
+        color={theme.palette.success.main}
       >
-        <CardActionArea>
-          <CardContent>
-            <Typography component="div">Total Active users</Typography>
-            <Typography component="div">+67</Typography>
-            <Typography gutterBottom variant="h6" component="div">
-              1000
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+        {stat}
+      </Typography>
+      <Typography variant="caption">
+        <span>{caption}</span>
+      </Typography>
+    </Paper>
   );
 }
--- apps/frontend/src/app/pages/public/fragments/FeaturedCourses.tsx
+++ apps/frontend/src/app/pages/public/fragments/FeaturedCourses.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardMedia,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { useQuery } from 'react-query';
 import { search } from '../../../api';
 import CourseCard from '../../../components/CourseCard';
@@ -9,13 +17,38 @@ export function FeaturedCourses() {
   return (
     <Box sx={{}}>
       <Container>
-        <Box sx={{ px: 3, pt: 3 }}>
-          <Typography variant="h6">Featured </Typography>
-        </Box>
         <Grid container sx={{ p: 3 }} spacing={2}>
+          <Grid item xs={3}>
+            <Box
+              sx={{
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'start',
+                justifyContent: 'end',
+              }}
+            >
+              <Typography variant="h6">
+                Most Popular
+                <br />
+                Courses
+              </Typography>
+              <Box sx={{ textAlign: 'right' }}>
+                <img
+                  src="/assets/images/illustration_5.png"
+                  style={{
+                    width: '100%',
+                    alignSelf: 'right',
+                    transform: 'scaleX(-1)',
+                  }}
+                  alt=""
+                />
+              </Box>
+            </Box>
+          </Grid>
           {data &&
             data.slice(0, 3).map((e: any) => (
-              <Grid item xs={4}>
+              <Grid item xs={3}>
                 <CourseCard course={e} />
               </Grid>
             ))}
