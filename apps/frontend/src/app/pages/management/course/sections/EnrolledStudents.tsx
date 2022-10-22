c93c78000a50089d64539d1ac856967af295c6f7
Ashan Hansaka <ashanhr@ashanhr.local>
2022-10-22 19:43:34 +0530

    add instructor model

--- apps/frontend/src/app/pages/management/course/sections/EnrolledStudents.tsx
+++ apps/frontend/src/app/pages/management/course/sections/EnrolledStudents.tsx
@@ -1,4 +1,17 @@
-import { Container, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,6 +22,7 @@ import TableRow from '@mui/material/TableRow';
 import { getOrgCourseUsers } from '../../../../api';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 //sample data array with objects containg username,email,enrolled date,course title
 const data = [
@@ -50,11 +64,10 @@ export default function EnrolledStudents() {
     getOrgCourseUsers(courseId ?? '')
   );
 
-  // console.log("metana metana metana");
-  // console.log(courseData);
   if (!courseData) return null;
   return (
     <TableContainer component={Paper}>
+      <AddInstructorForm />
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -64,19 +77,21 @@ export default function EnrolledStudents() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {courseData && courseData.length && courseData.map((row: any) => (
-            <TableRow key={row.enrollmentId}>
-              <TableCell sx={{ pl: theme.spacing(3) }}>
-                <Typography variant="body2">{row.user.name}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="body2">{row.user.email}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="body2">{row.enrollmentDate}</Typography>
-              </TableCell>
-            </TableRow>
-          ))}
+          {courseData &&
+            courseData.length &&
+            courseData.map((row: any) => (
+              <TableRow key={row.enrollmentId}>
+                <TableCell sx={{ pl: theme.spacing(3) }}>
+                  <Typography variant="body2">{row.user.name}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body2">{row.user.email}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body2">{row.enrollmentDate}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
       {/* <TablePagination
@@ -89,5 +104,60 @@ export default function EnrolledStudents() {
           onRowsPerPageChange={() => {}}
         /> */}
     </TableContainer>
+  );
+}
+
+function AddInstructorForm() {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'right', p: 2 }}>
+        <Box>
+          <Button color="primary" onClick={() => setOpen(true)}>
+            Add instructor
+          </Button>
+        </Box>
+      </Box>
+
+      {/* <Button variant="contained" onClick={() => setOpen(true)}>
+        Invite User
+      </Button> */}
+      <Dialog open={open} maxWidth="xs" hideBackdrop={false}>
+        <DialogTitle>
+          Add Instructor
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            We will send an email to the user asking them to join.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            // {...register('email')}
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions sx={{ mx: 2, mb: 2 }}>
+          <Button color="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            // onClick={() => {
+            //   inviteUserMutation.mutate({ email: getValues('email') });
+            // }}
+          >
+            Invite
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
