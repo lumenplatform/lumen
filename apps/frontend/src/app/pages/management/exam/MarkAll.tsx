d1860005713348f611f5e5351603a5666950fa8a
Udani Ranasinghe <prabodya99@gmail.com>
2022-10-23 21:29:45 +0530

    feat(quizmarking):MarkAll file changes and added submissionMarking file

--- apps/frontend/src/app/pages/management/exam/MarkAll.tsx
+++ apps/frontend/src/app/pages/management/exam/MarkAll.tsx
@@ -1,100 +1,63 @@
-import CheckIcon from '@mui/icons-material/Check';
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import {
-  Button,
-  Card,
-  Divider,
-  InputAdornment,
-  Skeleton,
-  Stack,
-  TableCell,
-  TableRow,
-  TextField,
+  Button, Skeleton, TableCell
 } from '@mui/material';
 import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
 import Typography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
-import { useMutation, useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-import { getAttemptById, getQuizById, markSubmssion } from '../../../api';
-import EssayQ from '../../../components/EssayQuiz';
-import MCQ from '../../../components/MCQQuiz';
-import Pagination from '@mui/material/Pagination';
-import { ArrowBack, ArrowForward } from '@mui/icons-material';
-import Table from '@mui/material/Table';
+import { useQuery } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getQuizById } from '../../../api';
+import SubmissionMarking from './SubmissionMarking';
 
-function MarkingBox(props: any) {
-  const { markEnabled, maxMarks, submissionId, marks = 0 } = props;
-  const [marksInput, setMarksInput] = useState(marks);
-  const { courseId, examId, attemptId } = useParams();
-  const { mutate: submissionMarkMutation } = useMutation(markSubmssion);
-  const mark = (mark: number) => {
-    submissionMarkMutation({
-      quizId: examId,
-      courseId: courseId,
-      attemptId: attemptId,
-      submissionId: submissionId,
-      mark: { submissionId: submissionId, marks: mark },
-    });
-  };
+export default function QuizMarkingAll() {
 
-  return (
-    <Box sx={{ m: 2, display: 'flex', justifyContent: 'end' }}>
-      <Stack direction="row" spacing={2} alignItems="center">
-        <Typography variant="subtitle1"> marks : </Typography>
-        <TextField
-          size="small"
-          disabled={!markEnabled}
-          defaultValue={marksInput}
-          type="number"
-          sx={{ width: '10ch', mr: 2 }}
-          InputProps={{
-            inputProps: { min: 0, max: maxMarks },
-            endAdornment: (
-              <InputAdornment position="end">/{maxMarks}</InputAdornment>
-            ),
-          }}
-          onChange={(e) => setMarksInput(parseInt(e.target.value))}
-        />
-        <Button endIcon={<CheckIcon />} onClick={() => mark(marksInput)}>
-          Save
-        </Button>
-      </Stack>
-    </Box>
-  );
-}
+  const { courseId, examId, questionId } = useParams();
+  const navigate = useNavigate();
 
-//dummy data for quiz array
-const quizArray = {
-  title: 'exam title',
-  id: '01',
-  questions: [
-    {
-      questionid: 'q01',
-      type: 'mcq',
-      question: 'question test iggiugiugiugiu',
-      answers: [
-        { answerid: 'a1', answer: 'nibgyigigi' },
-        { answerid: 'a2', answer: 'n2ryigigi' },
-        { answerid: 'a3', answer: 'nsgigigi' },
-      ],
-    },
-  ],
-};
+  const {
+    data: examData,
+    isLoading: isExamLoading,
+    isError: isExamError,
+  } = useQuery(['exam', examId], () => getQuizById(courseId!, examId!));
 
-export default function QuizMarking() {
+  const [nextQuestionId, setNextQuestionId] = useState<string | null>(null);
+  const [prevQuestionId, setPrevQuestionId] = useState<string | null>(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  useEffect(() => {
+    if (examData) {
+      const questionIndex = examData.questions.findIndex(
+        (question: any) => question.id === questionId
+      );
+      setCurrentQuestionIndex(questionIndex);
+      if (questionIndex !== -1) {
+        if (questionIndex !== 0)
+          setPrevQuestionId(examData.questions[questionIndex - 1].id);
+        else setPrevQuestionId(null);
+
+        if (questionIndex !== examData.questions.length - 1)
+          setNextQuestionId(examData.questions[questionIndex + 1].id);
+        else setNextQuestionId(null);
+      }
+    }
+  }, [examData, questionId]);
+
+  if (isExamLoading || isExamError) return <Skeleton/>;
+
   return (
     <Box sx={{ mb: 5 }}>
-      <Typography variant="h5">{'Exam title'}</Typography>
-      {/* <Typography variant="body1"> Attempt {'01'}</Typography> */}
-
+      <Typography variant="h5">{examData.settings.title}</Typography>
       {/* for two buttons to go to questions*/}
       <Table>
         <TableCell>
           <Button
             startIcon={<ArrowBack />}
-            // onClick ={()=> navigate(`/manage/courses/${courseId}/exam/${examId}/attempts`)}
+            onClick={() => navigate(`/manage/courses/${courseId}/exam/${examId}/question/${prevQuestionId}/submission`)}
             color="inherit"
             variant="contained"
+            disabled={prevQuestionId === null}
           >
             Previous Question
           </Button>
@@ -103,73 +66,17 @@ export default function QuizMarking() {
         <TableCell align="right">
           <Button
             startIcon={<ArrowForward />}
-            // onClick ={()=> navigate(`/manage/courses/${courseId}/exam/${examId}/attempts`)}
+            onClick={() => navigate(`/manage/courses/${courseId}/exam/${examId}/question/${nextQuestionId}/submission`)}
             color="inherit"
             variant="contained"
+            disabled={nextQuestionId === null}
           >
             Next Question
           </Button>
         </TableCell>
-        
+
       </Table>
-
-      {/* <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Box sx={{ minWidth: 800 }}>
-          {quizArray.questions.map((quiz: any, index: number, arr: any) => (
-            <Stack>
-              {quiz.type == 'essay' && (
-                <Card sx={{ marginTop: 2 }}>
-                  <EssayQ
-                    index={index + 1}
-                    noOfQuestions={arr.length}
-                    questionId={quiz.id}
-                    question={quiz.question}
-                    answer={quiz.submission.essayAnswer}
-                    disabled={true}
-                  />
-                  <Divider />
-                  <MarkingBox
-                    markEnabled={true}
-                    maxMarks={quiz.marks}
-                    marks={quiz.submission.marks}
-                    submissionId={quiz.submission.id}
-                  />
-                </Card>
-              )}
-              {quiz.type == 'mcq' && (
-                <Card sx={{ marginTop: 2 }}>
-                  <MCQ
-                    index={index + 1}
-                    noOfQuestions={arr.length}
-                    questionId={quiz.id}
-                    answers={quiz.answers}
-                    question={quiz.question}
-                    disabled={true}
-                  />
-                  <Divider />
-                  <MarkingBox
-                    markEnabled={false}
-                    maxMarks={quiz.marks}
-                    marks={quiz.submission.marks}
-                    submissionId={quiz.submission.id}
-                  />
-                </Card>
-              )}
-            </Stack>
-          ))}
-        </Box>
-      </Box> */}
-
-      <Stack spacing={10}>
-        <Pagination count={0} />
-      </Stack>
+      <SubmissionMarking index={currentQuestionIndex} noOfQuestions={examData.questions.length}/>
     </Box>
   );
 }
